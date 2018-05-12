e1511401e03d41b708bf580dd56d455069304e4f
tkrplus <niwa.buffer@gmail.com>
2018-05-12 23:44:46 +0900

    Github.js メソッド統一

--- src/github.js
+++ src/github.js
@@ -3,30 +3,23 @@ import GitHubScrapingClient from './utils/ScrapingClient/GitHubScrapingClient'
 const client = new GitHubScrapingClient()
 
 module.exports = ( robot => {
-  robot.respond(/ぎっとはぶ/, async msg => {
-    const result = await client.getGitHubActibity('tkrplus')
-
-    const msgText =
-      `GitHub活動実績\n` +
-      `今週は${result.week.days}日経過した中で \`${result.week.count}\` 回。 ${result.week.ratio.toFixed(1)}%\n` +
-      `今月では${result.month.days}日経過した中で \`${result.month.count}\` 回。 ${result.month.ratio.toFixed(1)}%\n` +
-      `今年では${result.year.days}日経過した中で \`${result.year.count}\` 回。 ${result.year.ratio.toFixed(1)}%\n` +
-      `${result.user.uri}`
-
-    msg.send(msgText)
+  robot.respond(/ぎっとはぶ/, msg => {
+    sendGitHubActibityResult('tkrplus', msg)
   })
 
-  robot.respond(/GitHub (.*)/, async msg => {
+  robot.respond(/GitHub (.*)/, msg => {
     const username = msg.match[1]
-    const result = await client.getGitHubActibity(username)
+    sendGitHubActibityResult(username, msg)
+  })
 
+  const sendGitHubActibityResult = async (username, msg) => {
+    const result = await client.getGitHubActibity(username)
     const msgText =
       `GitHub活動実績\n` +
       `今週は${result.week.days}日経過した中で \`${result.week.count}\` 回。 ${result.week.ratio.toFixed(1)}%\n` +
       `今月では${result.month.days}日経過した中で \`${result.month.count}\` 回。 ${result.month.ratio.toFixed(1)}%\n` +
       `今年では${result.year.days}日経過した中で \`${result.year.count}\` 回。 ${result.year.ratio.toFixed(1)}%\n` +
       `${result.user.uri}`
-
     msg.send(msgText)
-  })
+  }
 })
